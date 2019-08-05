8efc6617bae569c2d64193c9a53e673ffb3b9cee
sdanisman <dserkan0@Gmail.com>
2019-08-04 23:18:03 -0600

    negative test been add

--- src/test/java/Tests/LoginTest.java
+++ src/test/java/Tests/LoginTest.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -35,11 +36,18 @@ public class LoginTest{
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test"+ Keys.ENTER);
         driver.findElement(By.id("ctl00_logout")).click();
-        Assert.assertEquals(driver.getTitle(),"Web Orders Login");
 
 
     }
-    @AfterMethod
+    @Test
+    public  void NegativeTest() {
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete11/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("tests" + Keys.ENTER);
+        String ErorMassage=driver.findElement(By.id("ctl00_MainContent_status")).getText();
+        Assert.assertEquals(ErorMassage,"Invalid Login or Password.");
+    }
+        @AfterMethod
     public void cleanUp(){
         driver.close();
 
