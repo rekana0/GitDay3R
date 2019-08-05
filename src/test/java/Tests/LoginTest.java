d9641027f049fe6ab5a0990f1b35937f284c292b
sdanisman <dserkan0@Gmail.com>
2019-08-04 20:14:19 -0600

    Commit 2 after method

--- src/test/java/Tests/LoginTest.java
+++ src/test/java/Tests/LoginTest.java
@@ -6,7 +6,9 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -14,7 +16,7 @@ import java.util.concurrent.TimeUnit;
 public class LoginTest{
     WebDriver driver;
 
-    @BeforeClass
+    @BeforeMethod
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
@@ -26,5 +28,10 @@ public class LoginTest{
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test"+ Keys.ENTER);
         Assert.assertEquals(driver.getTitle(),"Web Orders");
+    }
+    @AfterMethod
+    public void logout(){
+        driver.close();
+
     }
 }
